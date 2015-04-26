741cf1bef4307e9261d06515928427898444d588
Cristiano Santos <cristianossd@gmail.com>
2015-04-26 18:43:24 -0300

    Making a depth search to match the paths

--- energy-transmission/main.c
+++ energy-transmission/main.c
@@ -3,20 +3,35 @@
 #include <stdbool.h>
 #include <string.h>
 
-int e, l;
-int test_num = 1;
+int e, l, label[101], test_num = 1;
 
-_Bool match_path(int from, int to) {
+void reach(int g[][e], int from) {
+  int v;
+
+  label[from] = 1;
+  for (v=0; v<e; v++)
+    if ((g[from][v] == 1 || g[v][from] == 1) && label[v] == 0)
+      reach(g, v);
+}
+
+_Bool match_path(int g[][e], int from, int to) {
+  int v;
+
+  for (v=0; v<e; v++)
+    label[v] = 0;
+  reach(g, from);
+
+  if (label[to] == 0)
+    return false;
   return true;
 }
 
-_Bool read_graph(int p[][e]) {
-  // To access the element of array: *(p + (line*2) + col)
+_Bool transmission_state(int g[][e]) {
   int i, j;
 
   for (i=0; i<e; i++) {
     for (j=0; j<e; j++) {
-      if (!match_path(i, j))
+      if (!match_path(g, i, j))
         return false;
     }
   }
@@ -24,19 +39,17 @@ _Bool read_graph(int p[][e]) {
   return true;
 }
 
-_Bool transmission_state() {
-  int i, j, x, y, pair[e][e];
+_Bool build_graph() {
+  int i, j, x, y, graph[e][e];
 
-  memset(pair, 0, sizeof(pair[0][0]) * e * e);
+  memset(graph, 0, sizeof(graph[0][0]) * e * e);
   // reading the input
   for (i=0; i<l; i++) {
     scanf("%d %d\n", &x, &y);
-    printf("Att %d:%d - ", x, y);
-    pair[x-1][y-1] = 1;
-    printf("%d:%d with %d\n", x-1, y-1, pair[x-1][y-1]);
+    graph[x-1][y-1] = 1;
   }
 
-  if (!read_graph(pair))
+  if (!transmission_state(graph))
     return false;
   return true;
 }
@@ -49,7 +62,7 @@ int main() {
        ((l < e-1)||(l > e*(e-1)/2)))
       exit(1);
 
-    if (transmission_state())
+    if (build_graph())
       printf("Teste %d\nnormal\n\n", test_num);
     else
       printf("Teste %d\nfalha\n\n", test_num);
