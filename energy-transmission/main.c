1f329120f2249e9ee851c22bab95e4b5f54f07c4
Cristiano Santos <cristianossd@gmail.com>
2015-04-26 16:07:58 -0300

    Passing a multi dimensional array

--- energy-transmission/main.c
+++ energy-transmission/main.c
@@ -4,12 +4,18 @@
 
 int e, l, i;
 
-void read_input() {
-  int x, y;
+void print(int *p) {
+  printf("%d %d\n", *(p + (i*2)), *(p + (i*2) + 1));
+}
 
+// To acess the the element: *(p + (line*2) + col)
+void transmission_state() {
+  int x, y, pair[l][2];
+
+  // reading the input
   for (i=0; i<l; i++) {
-    scanf("%d %d\n", &x, &y);
-    printf("%d-line of %d\n", i, l);
+    scanf("%d %d\n", &(pair[i][0]), &(pair[i][1]));
+    print(&pair[0][0]);
   }
 }
 
@@ -20,6 +26,6 @@ int main() {
        ((e < 3)||(e > 100)) ||
        ((l < e-1)||(l > e*(e-1)/2)))
       exit(1);
-    read_input();
+    transmission_state();
   }
 }
