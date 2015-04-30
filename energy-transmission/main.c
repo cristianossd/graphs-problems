e580e2f0e81513edd5c2cb97fefac6fa5067dd69
Cristiano Santos <cristianossd@gmail.com>
2015-04-30 09:29:02 -0300

    Updating the depth algorithm to find the paths for just one node

--- energy-transmission/main.c
+++ energy-transmission/main.c
@@ -2,24 +2,25 @@
 #include <stdbool.h>
 #include <string.h>
 
-int e, l, label[101], test_num = 1;
+int e, l, label[101], test_num = 1, visited = 0;
 
-int depth_search(int g[][e], int from, int to) {
+int depth_search(int g[][e], int from) {
   int node;
 
   label[from] = 1;
+  visited++;
   for (node=0; node<e; node++)
     if (g[from][node] == 1 && label[node] == 0)
-      depth_search(g, node, to);
+      depth_search(g, node);
 
-  return label[to] == 1;
+  return visited == e;
 }
 
 int build_graph() {
   int i, j, x, y;
   bool result;
 
-  scanf("%d %d\n", &e, &l);
+  scanf("%d %d", &e, &l);
   if (((e == 0)&&(l == 0)) ||
       ((e < 3)||(e > 100)) ||
       ((l < e-1)||(l > e*(e-1)/2)))
@@ -31,17 +32,12 @@ int build_graph() {
   memset(label, 0, sizeof(label[0]) * e);
   // reading the input
   for (i=0; i<l; i++) {
-    scanf("%d %d\n", &x, &y);
+    scanf("%d %d", &x, &y);
     graph[x-1][y-1] = 1;
     graph[y-1][x-1] = 1;
   }
 
-  for (i=0; i<e; i++) {
-    for (j=0; j<e; j++) {
-      if (!depth_search(graph, i, j))
-        result = false;
-    }
-  }
+  result = depth_search(graph, 0);
 
   if (result)
     printf("Teste %d\nnormal\n\n", test_num);
